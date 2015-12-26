6fa4ef1dd1e1a07abd0053c0fefff3b087810bb2
Chris Brown <chris@thebrown.net>
2015-12-26 13:53:37 +0000

    Restructured files to make deployment easier

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ To run:
     git clone https://github.com/octachrome/chordsolver.git
     cd chordsolver
     npm install
+    grunt
     node server.js
 
 Open `http://localhost:3000` in your browser, and enter the chord name or notes.
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/www'));
-app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname));
 
-var chordSearch = require('./www/chords').chordSearch;
+var chordSearch = require('./chords').chordSearch;
 
 app.get('/chords', function (req, res) {
     var chords = chordSearch(req.query.query);
